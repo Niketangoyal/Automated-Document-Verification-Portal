1d05563ed4ba249c4ce6a0c95582025543f7fc68
niketan <niketangoyal3@gmail.com>
2026-04-30 16:35:49 +0530

    chore: update outputFileTracingIncludes to include additional tesseract dependencies

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,26 @@ const nextConfig: NextConfig = {
     },
   },
   outputFileTracingIncludes: {
-    "/*": ["./node_modules/tesseract.js/**/*", "./node_modules/tesseract.js-core/**/*"],
-    "/api/**/*": ["./node_modules/tesseract.js/**/*", "./node_modules/tesseract.js-core/**/*"]
+    "/*": [
+      "./node_modules/tesseract.js/**/*",
+      "./node_modules/tesseract.js-core/**/*",
+      "./node_modules/bmp-js/**/*",
+      "./node_modules/is-url/**/*",
+      "./node_modules/node-fetch/**/*",
+      "./node_modules/regenerator-runtime/**/*",
+      "./node_modules/wasm-feature-detect/**/*",
+      "./node_modules/zlibjs/**/*"
+    ],
+    "/api/**/*": [
+      "./node_modules/tesseract.js/**/*",
+      "./node_modules/tesseract.js-core/**/*",
+      "./node_modules/bmp-js/**/*",
+      "./node_modules/is-url/**/*",
+      "./node_modules/node-fetch/**/*",
+      "./node_modules/regenerator-runtime/**/*",
+      "./node_modules/wasm-feature-detect/**/*",
+      "./node_modules/zlibjs/**/*"
+    ]
   }
 };
 
