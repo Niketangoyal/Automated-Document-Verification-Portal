e4a72350fffb7316eac7f758b37eb730ee792555
niketan <niketangoyal3@gmail.com>
2026-04-30 16:29:32 +0530

    chore: add tesseract.js-core to serverExternalPackages and update outputFileTracingIncludes configuration

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    serverExternalPackages: ["tesseract.js"],
+  serverExternalPackages: ["tesseract.js", "tesseract.js-core"],
   experimental: {
     serverActions: {
       bodySizeLimit: "20mb", // Increase the limit (set as per requirement)
     },
   },
- 
+  outputFileTracingIncludes: {
+    "/*": ["./node_modules/tesseract.js/**/*", "./node_modules/tesseract.js-core/**/*"],
+    "/api/**/*": ["./node_modules/tesseract.js/**/*", "./node_modules/tesseract.js-core/**/*"]
+  }
 };
 
 export default nextConfig;
